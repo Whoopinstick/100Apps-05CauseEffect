9d70a8b6fcf8f8235e175245f770c00a8e797155
Whoopinstick <k.m.paul68@gmail.com>
2020-08-04 21:46:09 -0400

    Add stock photos and JSON

---
+++ 05 - CauseEffect/View/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  05 - CauseEffect
+//
+//  Created by Kevin Paul on 8/4/20.
+//  Copyright © 2020 Whoopinstick. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
